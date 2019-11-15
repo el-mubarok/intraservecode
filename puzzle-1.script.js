e1dbdf846f8788ca8d9cb272c5cc85b4603e9631
Dwi Arif <mubarok13@gmail.com>
2019-11-15 17:37:23 +0700

    create puzzle-1.script

---
+++ puzzle-1.script.js
@@ -0,0 +1,57 @@
+// A javascript-enhanced crossword puzzle [c] Jesse Weisbeck, MIT/GPL 
+(function($) {
+	$(function() {
+		// provide crossword entries in an array of objects like the following example
+		// Position refers to the numerical order of an entry. Each position can have 
+		// two entries: an across entry and a down entry
+		var puzzleData = [
+			// across
+			 	{
+					clue: "Sebutan untuk orang barat atau turis",
+					answer: "bule",
+					position: 2,
+					orientation: "across",
+					startx: 1,
+					starty: 5
+				},
+				{
+					clue: "Jalan kecil yang biasanya hanya muat kendaraan roda dua",
+					answer: "gang",
+					position: 1,
+					orientation: "across",
+					startx: 3,
+					starty: 3
+				},
+				{
+					clue: "Baju perempuan bagain bawah (bawahan)",
+					answer: "rok",
+					position: 3,
+					orientation: "across",
+					startx: 6,
+					starty: 5
+				},
+
+			// down
+				{
+					clue: "Tempat nongkrong yang menyajikan kopi dan makanan ringan",
+					answer: "kafe",
+					position: 5,
+					orientation: "down",
+					startx: 4,
+					starty: 2
+				},
+				{
+					clue: "Mati dalam pertempuran",
+					answer: "gugur",
+					position: 4,
+					orientation: "down",
+					startx: 6,
+					starty: 1
+				}
+			] 
+	
+		$('#puzzle-wrapper').crossword(puzzleData);
+		
+	})
+	
+})(jQuery)
